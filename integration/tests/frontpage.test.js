5a7c702203ab6eece8631907f471f7c7aaff3de9
Martin Møller <mmj@dbc.dk>
2017-01-10 13:07:09 +0100

    Linting

--- integration/tests/frontpage.test.js
+++ integration/tests/frontpage.test.js
@@ -2,11 +2,11 @@ import {assert} from 'chai';
 
 describe('Testing frontpage', () => {
   beforeEach(() => {
-    browser.url('/');
+    browser.url('/'); // eslint-disable-line no-undef
   });
 
   it('Should render frontpage', () => {
-    const text = browser.getText('body');
+    const text = browser.getText('body'); // eslint-disable-line no-undef
     const expected = 'It\'s a bird...';
     assert.equal(text, expected);
   });
