cb6cb463d72f2fe145183e5d794cddc13e86d14c
Søren Vibjerg <vibjerg@gmail.com>
2017-01-10 12:16:10 +0100

    Add upload form container

---
+++ src/client/components/uploadForm/uploadFormContainer.component.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ImageUpload from './imageUpload.component';
+import PreviewContainer from './previewContainer.component';
+
+export default class UploadFormContainer extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.minSize = 1000000;
+    this.maxSize = 5000000;
+    this.accepts = 'image/jpeg, image/jpg, image/png';
+    this.state = {
+      acceptedFiles: [],
+      rejectedFiles: []
+    };
+  }
+
+  onDrop(acceptedFiles, rejectedFiles) {
+    this.setState({
+      acceptedFiles: this.state.acceptedFiles.concat(acceptedFiles),
+      rejectedFiles: this.state.rejectedFiles.concat(rejectedFiles)
+    });
+  }
+
+  render() {
+    return (
+      <div className="upload-form-container">
+        <ImageUpload accept={this.accepts} minSize={this.minSize} maxSize={this.maxSize} onDrop={(acceptedFiles, rejectedFiles) => this.onDrop(acceptedFiles, rejectedFiles)} />
+      </div>
+    );
+  }
+}
