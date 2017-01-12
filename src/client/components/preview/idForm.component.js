966d883948a8c613039422635f6718167095a668
Søren Vibjerg <vibjerg@gmail.com>
2017-01-12 13:56:27 +0100

    add id input form

---
+++ src/client/components/preview/idForm.component.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function IdForm({value, onSubmit}) {
+  let refInput;
+  return (
+    <div className="id-form">
+      <label htmlFor="id">ID</label>
+      <input ref={(input) => refInput = input} id="id" type="text" name="id" defaultValue={value}/>
+      <button onClick={() => onSubmit(refInput.value)}>Opdater</button>
+    </div>
+  );
+}
+
+IdForm.propTypes = {
+  value: React.PropTypes.string,
+  onSubmit: React.PropTypes.func
+};
