371963daac53a6187fe7325c2ecd59048b080e32
Søren Vibjerg <vibjerg@gmail.com>
2017-01-13 11:19:58 +0100

    Add tests

--- integration/pages/urlUpload.page.js
+++ integration/pages/urlUpload.page.js
@@ -7,4 +7,10 @@ export default class UrlUpladPage {
     browser.click('.url-upload button');
     browser.waitForValue('.preview.component', 5000);
   }
+  submitId(id) {
+    browser.setValue('.id-form input', id);
+    browser.click('.id-form button');
+    browser.waitForValue('.work-preview', 5000);
+    return browser.element('.work-preview');
+  }
 }
---
+++ integration/tests/workInfo.test.js
@@ -0,0 +1,29 @@
+/* eslint no-undef: 0 */
+import {assert} from 'chai';
+import UrlUpladPage from '../pages/urlUpload.page';
+
+describe('Testing url upload component', () => {
+  const page = new UrlUpladPage();
+  beforeEach(() => {
+    page.open();
+  });
+
+
+  it('Should show work information', () => {
+    page.uploadUrls('https://www.colourbox.dk/preview/2582621-white-horses-grazing-on-ranch.jpg');
+    const element = page.submitId(12345).getText();
+    assert.include(element, 'Titel');
+    assert.include(element, 'Ophav');
+    assert.include(element, 'Materiale type');
+    assert.include(element, 'ISBN');
+  });
+
+  it('Should compare existing image with new image', () => {
+    page.uploadUrls('https://www.colourbox.dk/preview/2582621-white-horses-grazing-on-ranch.jpg');
+    page.submitId(12345);
+    const previewImage = browser.element('.preview-image').getText();
+    assert.include(previewImage, 'Ny');
+    assert.include(previewImage, 'Eksisterende');
+  });
+
+});
