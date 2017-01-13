a1ca3c22d2aac30711bcbf3bb0f77b049c0b7b08
Søren Vibjerg <vibjerg@gmail.com>
2017-01-13 10:39:56 +0100

    Add reason for rejection of file

--- src/client/components/imageUpload/imageUploadContainer.component.js
+++ src/client/components/imageUpload/imageUploadContainer.component.js
@@ -15,10 +15,27 @@ export default class ImageUploadContainer extends React.Component {
     };
   }
 
+  rejectedReason = (rejectedFile) => {
+    let reason = 'Filen er ugyldig';
+    if (!rejectedFile.type || !this.accepts.includes(rejectedFile.type)) {
+      reason = 'Filen er ikke en gyldig type. Det skal være en png eller en jpg';
+    }
+    else if (rejectedFile.size < this.minSize || rejectedFile.size > this.maxSize) {
+      reason = `Filen har ikke en gyldig størrelse. Den skal være minimum ${this.minSize / 1000000}MB og maksimalt ${this.maxSize / 1000000}MB`;
+    }
+
+    rejectedFile.message = {
+      type: 'error',
+      text: reason
+    };
+
+    return rejectedFile;
+  };
+
   onDrop = (acceptedFiles, rejectedFiles) => {
     this.setState({
       accepted: this.state.accepted.concat(acceptedFiles),
-      rejected: this.state.rejected.concat(rejectedFiles)
+      rejected: this.state.rejected.concat(rejectedFiles.map(this.rejectedReason))
     });
   };
 
