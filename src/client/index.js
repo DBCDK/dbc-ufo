ff7b0635579221d8b6a63cee7b0a7201574733c2
Vibjerg <vibjerg@gmail.com>
2017-01-16 22:29:47 +0100

    Add url upload to index to make test go green

--- src/client/index.js
+++ src/client/index.js
@@ -11,7 +11,8 @@ import './scss/index.scss';
 
 // Components
 import UploadImageContainer from './components/imageUpload/imageUploadContainer.component';
+import UploadUrlontainer from './components/urlUpload/urlUploadContainer.component';
 
 const rootContainer = document.getElementById('content');
 
-ReactDOM.render(<div><UploadImageContainer /></div>, rootContainer);
+ReactDOM.render(<div><UploadImageContainer /><UploadUrlontainer/></div>, rootContainer);
