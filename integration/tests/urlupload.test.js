581e13d60cb330496f505d473f520c8899696ddb
Søren Vibjerg <vibjerg@gmail.com>
2017-01-18 11:33:42 +0100

    fix tests

--- integration/tests/urlupload.test.js
+++ integration/tests/urlupload.test.js
@@ -9,7 +9,7 @@ describe('Testing url upload component', () => {
   });
 
   it('Should render component', () => {
-    const element = $('.upload-urls-container');
+    const element = $('.url-upload');
     element.waitForExist(5000);
     assert.include(element.getText(), 'VIS I LISTE');
   });
