36bf90ce6e1b7134eaa0496fa66d585e13de3c0f
Martin Møller <mmj@dbc.dk>
2017-01-18 22:31:21 +0100

    prepping forsrights login to be tested on liveservice on internal CI

--- integration/utils/signin.util.js
+++ integration/utils/signin.util.js
@@ -9,9 +9,9 @@
  */
 browser.signIn = () => {
   browser.url('/login');
-  browser.setValue('#login-input-agency', 'somevalue');
-  browser.setValue('#login-input-user', 'somevalue');
-  browser.setValue('#login-input-password', 'somevalue');
+  browser.setValue('#login-input-agency', process.env.FORS_RIGHTS_URI_TEST_AGENCY);
+  browser.setValue('#login-input-user', process.env.FORS_RIGHTS_URI_TEST_USER);
+  browser.setValue('#login-input-password', process.env.FORS_RIGHTS_URI_TEST_PASSWORD);
   browser.click('#login-input-tac');
   browser.click('#login-input-submit');
   browser.pause(300);
