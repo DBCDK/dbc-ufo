216510bde51fde585d47234705fc99388f06b159
Søren Vibjerg <vibjerg@gmail.com>
2017-02-03 15:42:17 +0100

    Add message if images are rejected

--- src/client/components/preview/previewList.component.js
+++ src/client/components/preview/previewList.component.js
@@ -24,6 +24,7 @@ export default class PreviewList extends React.Component {
           <h2>Preview af billeder og materialer</h2>
           <p>Gennemse uploadede billeder. Alle billeder skal have tilknyttet et gyldigt materiale ID, som billedet skal
             tilknyttes</p>
+          {this.props.rejected.length && (<p className="message"><span className="nb">Bemærk</span> Der er fejl i listen af uploadede billeder</p>) || ''}
         </div>
         <div className="upload-button text-center grow">
           <button className="submit large" disabled={!State.readyForUpload()} onClick={State.upload}><span className="icon icon-inline upload" /> Start upload
