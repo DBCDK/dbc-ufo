f90b040f62f9e5c965f7f219deef5099bf35d0ef
Søren Vibjerg <vibjerg@gmail.com>
2017-02-08 10:49:00 +0100

    fix test

--- integration/pages/upload.page.js
+++ integration/pages/upload.page.js
@@ -36,7 +36,7 @@ export default class UploadPage {
   rejectId(id) {
     browser.setValue('.id-input input', id);
     browser.click('.id-form button');
-    browser.waitForValue('.message', 5000);
+    browser.waitForValue('.status .error', 5000);
     return browser.element('.status');
   }
 
