8a92fb9f70591470cb0935cc403160083d0362cb
Finn Stausgaard <fvs@dbc.dk>
2017-08-23 10:59:12 +0200

    Remove comment. No alert is shown when adding image

--- integration/tests/imageupload.test.js
+++ integration/tests/imageupload.test.js
@@ -18,7 +18,6 @@ describe('Testing image upload', () => {
 
   it('Should show preview of image', () => {
     page.addImage('horses.jpg');
-    // page.clearAlert();
   });
 
   it('Should reject non image', () => {
