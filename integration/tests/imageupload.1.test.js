ecd4f146dbf581aab3723b0d3582cc43f9e969ba
Finn Stausgaard <fvs@dbc.dk>
2017-08-28 09:33:03 +0200

    Split test into 3

---
+++ integration/tests/imageupload.1.test.js
@@ -0,0 +1,34 @@
+/* eslint no-undef: 0 */
+import {assert} from 'chai';
+import UploadPage from '../pages/upload.page';
+
+describe('Testing image upload 1', () => {
+  const page = new UploadPage();
+
+  beforeEach(() => {
+    browser.signIn();
+    browser.selectImageUpload();
+  });
+
+  it('Should render component', () => {
+    const element = $('.upload-form');
+    element.waitForExist(5000);
+    assert.include(element.getText(), 'Træk filer hertil');
+  });
+
+  it('Should show preview of image', () => {
+    page.addImage('horses.jpg');
+    page.clearAlert();
+  });
+
+  it('Should reject non image', () => {
+    page.rejectImage('invalid_horse.txt');
+    assert.include(browser.element('.message').getText(), 'BEMÆRK Følgende billeder kan ikke benyttes', 'error message is shown');
+  });
+
+  it('Should upload image', () => {
+    page.addImage('horses.jpg');
+    page.submitId('9788792813114');
+    page.upload();
+  });
+});
